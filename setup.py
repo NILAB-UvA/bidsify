5acee1a922c815fe565672c2c59bf3fcab968fb2
Lukas Snoek <lukassnoek@gmail.com>
2016-04-18 13:36:35 +0200

    Create package structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import spynoza
+
+VERSION = spynoza.__version__
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='spynoza',
+    version=VERSION,
+    description='Python package for fMRI data processing',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'],
+    keywords="fMRI nipype preprocessing",
+    url='https://github.com/spinoza-centre',
+    author='Spinoza centre',
+    license='MIT',
+    platforms='Linux',
+    packages=find_packages(),
+    zip_safe=False)
