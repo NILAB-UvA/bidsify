2e7ec1e56d45a0cccc070b4fbf5a7bd86f680f07
Lukas Snoek <lukassnoek@gmail.com>
2016-06-09 18:15:35 +0200

    Change to one function call per logfile (instead of inputting list)

--- behav2tsv.py
+++ behav2tsv.py
@@ -1,12 +1,11 @@
 from __future__ import print_function
-import json
 import os
 import os.path as op
+import json
 import pandas as pd
 import numpy as np
 from glob import glob
-from nipype.interfaces.base import Bunch
-# Presentation-logfile converter
+from copy import copy, deepcopy
 
 class PresentationLogfileCrawler(object):
     """ Parses a Presentation logfile.
@@ -16,30 +15,13 @@ class PresentationLogfileCrawler(object):
     To do: extract parametric weight is specified
     """
 
-    def __init__(self, in_file, con_names, con_codes, con_duration=None,
-                 pulsecode=30, verbose=True):
-
-        # in_file can be a single file or list of files
-        if isinstance(in_file, str):
-            in_file = [in_file]
+    def __init__(self, in_file, event_dir, verbose=True):
 
         self.in_file = in_file
-        self.con_names = con_names
-        self.con_codes = con_codes
-
-        # Also con_duration needs to be converted to list
-        if con_duration is not None:
-
-            if isinstance(con_duration, (int, float)):
-                con_duration = [con_duration]
-
-            # If con_duration is the same for all conditions, expand
-            if len(con_duration) < len(con_names):
-                con_duration = con_duration * len(con_names)
-
-        self.con_duration = con_duration
-        self.pulsecode = pulsecode
         self.verbose = verbose
+        self.event_dir = event_dir
+        self.cfg = None
+        self.pulsecode = None
         self.df = None
         self.to_write = None
         self.base_dir = None
@@ -49,23 +31,47 @@ class PresentationLogfileCrawler(object):
         # set self.df to cleaned dataframe
         pass
 
+    def _load_task_info(self):
+        fn_pairs = op.basename(self.in_file).split('_')
+        task_id = [p.split('-')[-1] for p in fn_pairs if 'task' in p][0]
+
+        cfg_files = glob(op.join(self.event_dir, '*.json'))
+        cfg = [c for c in cfg_files if task_id in c]
+        if not cfg or len(cfg) > 1:
+            msg = 'Not a single task.json file found! Found: %r' % cfg
+            raise ValueError(msg)
+
+        with open(cfg[0]) as tmp:
+            self.cfg = json.load(tmp)
+
+    def _convert_to_range(self):
+
+        self.cfg['con_codes'] = [range(c[0], c[1]) for c in self.cfg['con_codes']]
+
     def parse(self):
+        self._load_task_info()
+        self._convert_to_range()
 
-        subject_info_list = [self._parse(f) for f in self.in_file]
+        con_names = self.cfg['con_names']
+        con_codes = self.cfg['con_codes']
+        con_durations = self.cfg['con_durations']
+        con_durations = None if con_durations == "" else con_durations
 
-        if len(subject_info_list) == 1:
-            return subject_info_list[0]
-        else:
-            return subject_info_list
+        try:
+            pulsecode = self.cfg['pulsecode']
+        except KeyError:
+            pulsecode = 30
 
-    def _parse(self, f):
+        if con_durations is not None:
 
-        if self.verbose:
-            print('Processing %s' % f)
+            if isinstance(con_durations, (int, float)):
+                con_durations = [con_durations]
 
-        # Remove existing .bfsl files
-        self.base_dir = op.dirname(f)
-        _ = [os.remove(x) for x in glob(op.join(self.base_dir, '*.bfsl'))]
+            if len(con_durations) < len(con_names):
+                con_durations = con_durations * len(con_names)
+
+        f = self.in_file
+        base_dir = op.dirname(f)
 
         # If .clean() has not been called (and thus logfile hasn't been loaded,
         # load in the logfile now.
@@ -74,8 +80,7 @@ class PresentationLogfileCrawler(object):
         else:
             df = pd.read_table(f, sep='\t', skiprows=3,
                                skip_blank_lines=True)
-
-        # Clean up unnecessary columns
+        # Clean up unnecessary columns (use list-compr to check if col exists)
         to_drop = ['Uncertainty', 'Subject', 'Trial', 'Uncertainty.1', 'ReqTime',
                    'ReqDur', 'Stim Type', 'Pair Index']
         _ = [df.drop(col, axis=1, inplace=True) for col in to_drop if col in df.columns]
@@ -83,20 +88,20 @@ class PresentationLogfileCrawler(object):
         # Ugly hack to find pulsecode, because some numeric codes are written as str
         df['Code'] = df['Code'].astype(str)
         df['Code'] = [np.float(x) if x.isdigit() else x for x in df['Code']]
-        pulse_idx = np.where(df['Code'] == self.pulsecode)[0]
+        pulse_idx = np.where(df['Code'] == pulsecode)[0]
 
-        if len(pulse_idx) > 1: # take first pulse if multiple pulses are logged
+        if len(pulse_idx) > 1:  # take first pulse if multiple pulses are logged
             pulse_idx = int(pulse_idx[0])
 
         # pulse_t = absolute time of first pulse
-        pulse_t = df['Time'][df['Code'] == self.pulsecode].iloc[0]
+        pulse_t = df['Time'][df['Code'] == pulsecode].iloc[0]
         df['Time'] = (df['Time'] - float(pulse_t)) / 10000.0
         df['Duration'] = df['Duration'] / 10000.0
 
         df_list = []
 
         # Loop over condition-codes to find indices/times/durations
-        for i, code in enumerate(self.con_codes):
+        for i, code in enumerate(con_codes):
 
             to_write = pd.DataFrame()
 
@@ -126,7 +131,7 @@ class PresentationLogfileCrawler(object):
             # Generate dataframe with time, duration, and weight given idx
             to_write['onset'] = df['Time'][idx]
 
-            if self.con_duration is None:
+            if con_durations is None:
                 to_write['duration'] = df['Duration'][idx]
                 n_nan = np.sum(np.isnan(to_write['duration']))
                 if n_nan > 1:
@@ -135,23 +140,21 @@ class PresentationLogfileCrawler(object):
 
                 to_write['duration'] = [np.round(x, decimals=2) for x in to_write['duration']]
             else:
-                to_write['duration'] = [self.con_duration[i]] * idx.sum()
+                to_write['duration'] = [con_durations[i]] * idx.sum()
 
             to_write['weight'] = np.ones((np.sum(idx), 1))
-            to_write['trail_type'] = [self.con_names[i] for j in range(idx.sum())]
+            to_write['trail_type'] = [con_names[i] for j in range(idx.sum())]
 
             df_list.append(pd.DataFrame(to_write))
 
         df = pd.concat(df_list).sort_values(by='onset', axis=0)
         fn = op.join(op.dirname(f), op.splitext(op.basename(f))[0])
         df.to_csv(fn + '.tsv', sep='\t', index=None)
+        os.remove(self.in_file)
 
 if __name__ == '__main__':
 
-    ex_dir = op.join(op.dirname(op.dirname(op.abspath(__file__))), 'examples')
-    test_log = op.join(ex_dir, 'data', 'example_preslog.log')
-    con_names = ['A', 'B', 'C']
-    con_codes = [range(100, 200),range(200, 300), range(300, 400)]
-    plc = PresentationLogfileCrawler(in_file=test_log, con_names=con_names,
-                                     con_codes=con_codes)
+    test_log = '/home/lukas/test_bids/sub-002/func/sub-002_task-TaskOne_acq-multiband_events.log'
+    events_dir = '/home/lukas/test_bids/task_info'
+    plc = PresentationLogfileCrawler(in_file=test_log, event_dir=events_dir)
     plc.parse()
---
+++ nifti_converters.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import os
+import os.path as op
+import pandas as pd
+import numpy as np
+from glob import glob
+
