646c42d389d32c3e17cc6feab6e28bf60c6c8586
Lukas Snoek <lukassnoek@gmail.com>
2016-10-05 09:41:21 +0200

    Add BidsConverter subfolder

--- spynoza/nodes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .filtering import apply_sg_filter
-from .utils import get_scaninfo
-from .motion_correction import find_middle_run, mcflirt_across_runs
-
-__all__ = ['apply_sg_filter', 'get_scaninfo', 'find_middle_run',
-           'mcflirt_across_runs']
