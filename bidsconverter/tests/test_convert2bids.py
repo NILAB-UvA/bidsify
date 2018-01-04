8c337ccf3573334665191cafc6906e65215b2a0e
Lukas Snoek <lukassnoek@gmail.com>
2018-01-04 15:31:57 +0100

    Fix deps

--- bidsconverter/tests/test_convert2bids.py
+++ bidsconverter/tests/test_convert2bids.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, division, print_function
 import os.path as op
 import numpy as np
-import pandas as pd
 import numpy.testing as npt
 import bidsconverter as bc
 
