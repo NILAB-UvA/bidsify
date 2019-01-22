c55e2e0b64a5d3a3d5812b71beae45fa352af67b
Lukas Snoek <lukassnoek@gmail.com>
2019-01-22 12:14:34 +0100

    Fix B0-renaming + fix headers of manually stopped scans

--- bidsify/mri2nifti.py
+++ bidsify/mri2nifti.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, division
 import os
+import warnings
 import os.path as op
 from glob import glob
 from .utils import check_executable, _compress, _run_cmd
@@ -22,6 +23,10 @@ def convert_mri(directory, cfg):
     if mri_ext in ['PAR', 'dcm']:
         mri_files = glob(op.join(directory, '*.%s' % mri_ext))
         for f in mri_files:
+
+            if '.PAR' in f:
+                _fix_header_manually_stopped_scan(f)
+
             basename, ext = op.splitext(op.basename(f))
             par_cmd = base_cmd + " -f %s %s" % (basename, f)
             # if debug, print dcm2niix output
@@ -60,7 +65,9 @@ def convert_mri(directory, cfg):
             _compress(nii)
             os.remove(nii)
 
-    _rename_phasediff_files(directory, idf='phasediff')
+    if 'fmap' in cfg.keys():
+        idfs = [elem['id'] for elem in cfg['fmap'].values()]
+        _rename_phasediff_files(directory, idf=idfs)
 
 
 def _rename_phasediff_files(directory, idf='phasediff'):
@@ -68,33 +75,84 @@ def _rename_phasediff_files(directory, idf='phasediff'):
     appends (or sometimes prepends) '_ph' to the filename after conversion.
     """
 
-    b0_files = glob(op.join(directory, '*%s*' % idf))
+    if not isinstance(idf, list):
+        idf = [idf]
 
+    b0_files = []
+    for this_idf in idf:
+        b0_files += sorted(glob(op.join(directory, '*%s*' % this_idf)))
+    
     for f in b0_files:
-        # Old version of dcm2niix
-        new_name = f.replace('_phMag.json', '_phasediff.json')
-        new_name = new_name.replace('_phMag_1', '_phasediff')
-        new_name = new_name.replace('_phMag_2', '_magnitude1')
+        if 'real' in op.basename(f):
+            os.rename(f, f.replace('real', 'phasediff')) 
+        else: 
+            if '.nii.gz' in f:
+                os.rename(f, f.replace('.nii.gz', 'magnitude1.nii.gz')) 
+            else:
+                os.rename(f, f.replace('.', 'magnitude1.'))
 
-        # New version of dcm2niix
-        new_name = new_name.replace('e1', 'magnitude1')
-        new_name = new_name.replace('e2', 'phasediff')
 
-        os.rename(f, new_name)
+def _fix_header_manually_stopped_scan(par):
 
-    # Hack for latest dcm2niix version
-    real_f = [f for f in b0_files if 'real' in f]
-    if len(real_f) > 2:
-        msg = "Found %i B0-files with 'real' in its filename: %s" % (len(real_f), real_f,)
-        raise ValueError(msg)
-    elif len(real_f) == 2:
-        json_file = [f for f in real_f if '.json' in f][0]
-        phasediff_file = [f for f in real_f if '.json' not in f][0]
-        magn_file = [f for f in b0_files if 'real' not in f and '.json' not in f][0]
-        magn_json_f = [f for f in b0_files if 'real' not in f and 'json' in f][0]
-        
-        os.remove(magn_json_f)
-        os.rename(magn_file, magn_file.replace('phasediff', 'magnitude1'))
-        os.rename(phasediff_file, phasediff_file.replace('_real', ''))
-    else:
-        pass  # just skip it
+    with open(par, 'r') as f:
+        lines = f.readlines()
+
+    found = False
+    for line in lines:
+        found = 'Max. number of slices/locations' in line
+        if found:
+            n_slices = line.split(':')[-1].strip().replace('\n', '')
+            break
+
+    if not found:
+        raise ValueError("Could not determine number of slices from PAR header (%s)!" % par)
+
+    found = False
+    for line_nr_of_dyns, line in enumerate(lines):
+        found = 'Max. number of dynamics' in line
+        if found:
+            n_dyns = line.split(':')[-1].strip().replace('\n', '')
+            break
+
+    if int(n_dyns) == 1:
+        return
+
+    if not found:
+        raise ValueError("Could not determine number of slices from PAR header (%s)!" % par)
+    
+    skip_rows_eof = 2
+    last_slice_nr = [char for char in lines[-(1+skip_rows_eof)].split(' ') if char][0]
+    if not last_slice_nr == n_slices:
+        warnings.warn("Number of slices not equal to expected number; "
+                      "Scan probably manually stopped. Attempt to fix it ...")
+
+        lines_to_drop = []
+        for i, line in enumerate(lines[::-1][skip_rows_eof:]):
+            slc = [char for char in line.split(' ') if char][0]
+            if slc != n_slices:
+                #print("Dropping %s" % (line))
+                lines_to_drop.append(line)
+            else:
+               break
+
+        for to_drop in lines_to_drop:
+            lines.pop(lines.index(to_drop))
+
+    found = False
+    for line_nr_of_start_slices, line in enumerate(lines):
+        found = '# === IMAGE INFORMATION =' in line
+        if found:
+            line_nr_of_start_slices += 2
+            break
+    
+    slice_lines = lines[line_nr_of_start_slices:-(skip_rows_eof+1)]
+    actual_dyns = len(slice_lines) / int(n_slices)
+
+    if actual_dyns != int(n_dyns):
+        warnings.warn("Number of dynamics in PAR-file %s (%s) do not match actual "
+                      "dynamics (%i); fixing this ..." % (par, n_dyns, actual_dyns))
+        lines[line_nr_of_dyns] = lines[line_nr_of_dyns].replace(n_dyns, str(int(actual_dyns)))
+
+    with open(par, 'w') as f_out:
+        [f_out.write(line) for line in lines]
+
